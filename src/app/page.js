6ac790aacdbed824e3627466a4cf9f9015b5dddd
fermarino <119690095+fermarino@users.noreply.github.com>
2023-08-18 18:30:03 -0300

    feat: search location by name

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
-import Image from 'next/image'
+'use client'
+import { useState } from 'react';
+import WeatherCard from '@/components/WeatherCard';
+import SearchBox from '@/components/SearchBox';
 
 export default function Home() {
+  const [selectedLocation, setSelectedLocation] = useState('Recife');
+
+  const handleLocationChange = (location) => {
+    setSelectedLocation(location);
+  };
+
   return (
-    <h1>Weather.</h1>
-  )
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-gradient-to-b from-slate-100 to-slate-200">
+      <SearchBox onLocationChange={handleLocationChange} />
+      <WeatherCard location={selectedLocation} />
+    </div>
+  );
 }
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,13 +1,23 @@
+'use client'
+import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
-import WeatherCard from './WeatherCard';
 
-const SearchBox = () => {
+
+const SearchBox = ({onLocationChange}) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onLocationChange(inputValue)
+  }
 
   return (
     <div className="flex flex-col items-center justify-center p-4">
-      <form className="mb-4">
+      <form className="mb-4" onSubmit={handleSubmit}>
         <div className="relative">
           <input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
             type="text"
             placeholder="Digite a cidade..."
             className="border rounded-lg py-2 px-4 w-70 sm:w-80 md:w-96 focus:outline-none focus:ring-2 focus:ring-blue-500 border-slate-200 shadow-md"
