c0a7fc1c13901a1aebd8f5623df4f74b9bbb3d0a
fermarino <119690095+fermarino@users.noreply.github.com>
2023-08-18 20:52:00 -0300

    feat: search by location

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -10,14 +10,14 @@ const WeatherCard = ({ location }) => {
     return <p>Carregando...</p>;
   }
 
-  const { name, main, weather } = data;
+  const { name, main, weather, sys } = data;
   const temperature = Math.round(main.temp);
   const weatherDescription = weather[0].description;
   console.log(data)
 
   return (
     <div className="weather-card max-h-[500px] bg-gradient-to-b from-blue-500 to-blue-800 rounded-lg p-8 text-white shadow-lg sm:max-w-[400px] lg:max-w-[400px]">
-      <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold mb-2">{name}</h2>
+      <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold mb-2">{name}, {sys.country}</h2>
       <p className="text-sm md:text-base text-slate-950 mb-4">Hoje, {new Date().toLocaleDateString()}</p>
       <div className="flex-col flex mt-10 gap-11">
         <div className="flex flex-col items-center justify-center mb-6">
