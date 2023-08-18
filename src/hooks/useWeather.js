0e82b97a2d5b7354ad580cf1f58e20eabd1a9990
fermarino <119690095+fermarino@users.noreply.github.com>
2023-08-18 20:53:08 -0300

    fix: async on fetch weather

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,23 +1,27 @@
-'use client'
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const useWeather = (location) => {
   const [data, setData] = useState(null);
-  
+
   useEffect(() => {
-    const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
-    fetch(url)
-      .then(response => response.json())
-      .then(jsonData => {
+    const fetchWeatherData = async () => {
+      try {
+        const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
+        const response = await fetch(url);
+        const jsonData = await response.json();
         setData(jsonData);
-      })
-      .catch(error => {
+      } catch (error) {
         console.error('Error fetching weather data:', error);
-      });
+      }
+    };
+
+    if (location) {
+      fetchWeatherData();
+    }
   }, [location]);
 
   return { data };
-}
+};
 
 export default useWeather;
